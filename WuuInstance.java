6c49dc6caf7176f9317f6b4fce109ea44bb5633b
UuqV <krlovell017@gmail.com>
2017-10-10 13:39:26 -0400

    Send and receive messages

--- WuuInstance.java
+++ WuuInstance.java
@@ -1,63 +1,42 @@
 import java.io.*;
 import java.net.*;
 
-public class EchoClient {
-	public static void main(String args[]) {
-		serverPort = Integer.parseInt(args[0]);
-		
-	}
-		
-	public void serverSocket(Integer port) throws IOException {
-		if (args.length != 1) {
-			System.err.println("Usage: java EchoServer <port number>");
-			System.exit(1);
-		}
+public class WuuInstance {
+	ServerSocket socket;
+	Socket clients;
+	PrintWriter sendToClient;
+	BufferedReader receiveFromClient;
+	
+	public void WuuInstance(Integer port) throws IOException {
 		int portNumber = port;
 		try (
-			ServerSocket serverSocket =
-			new ServerSocket(port);
+			ServerSocket serverSocket = new ServerSocket(port);
+		) {
+			socket = serverSocket;
+		} catch (IOException e) {
+			System.out.printl("Exception caught while attempting to listen on port " + port);
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	public listen() {
+		try (
 			Socket clientSocket = serverSocket.accept();     
-			PrintWriter out =
-				new PrintWriter(clientSocket.getOutputStream(), true);                   
-			BufferedReader in = new BufferedReader(
-				new InputStreamReader(clientSocket.getInputStream()));
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			) {
+				clients = clientSocket;
+				sendToClient = out;
+				receiveFromClient = in;
+				
 				String inputLine;
 				while ((inputLine = in.readLine()) != null) {
 					out.println(inputLine);
 				}
 			} catch (IOException e) {
-				System.out.println("Exception caught when trying to listen on port "
-					+ portNumber + " or listening for a connection");
+				System.out.println("Exception caught listening for a connection to server on port " + port);
 				System.out.println(e.getMessage());
 			}
 		}
-
-	public void clientSocket(Integer serverName, Integer serverPort) throws IOException {
-		if (args.length != 2) {
-			System.err.println("Usage: java EchoClient <host name> <port number>");
-			System.exit(1);
-		}
-
-		try (
-			Socket echoSocket = new Socket(serverName, serverPort);
-			PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader( new InputStreamReader(echoSocket.getInputStream()));
-			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-			) {
-				String userInput;
-				while ((userInput = stdIn.readLine()) != null) {
-					out.println(userInput);
-					System.out.println("echo: " + in.readLine());
-				}
-			} catch (UnknownHostException e) {
-				System.err.println("Don't know about host " + serverName;
-				System.exit(1);
-			} catch (IOException e) {
-				System.err.println("Couldn't get I/O for the connection to " +
-                serverPort);
-				System.exit(1);
-			}
-		} 
-	}
+	
 }
