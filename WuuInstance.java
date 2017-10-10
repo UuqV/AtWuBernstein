18d0e8ba6f097c5017a8db9db20e39aa9ffe17b1
UuqV <krlovell017@gmail.com>
2017-10-10 13:42:55 -0400

    Mild compilation fixes

--- WuuInstance.java
+++ WuuInstance.java
@@ -3,25 +3,26 @@ import java.net.*;
 
 public class WuuInstance {
 	ServerSocket socket;
+	int port;
 	Socket clients;
 	PrintWriter sendToClient;
 	BufferedReader receiveFromClient;
 	
-	public void WuuInstance(Integer port) throws IOException {
-		int portNumber = port;
+	public void WuuInstance(Integer portNumber) throws IOException {
+		port = portNumber;
 		try (
 			ServerSocket serverSocket = new ServerSocket(port);
 		) {
 			socket = serverSocket;
 		} catch (IOException e) {
-			System.out.printl("Exception caught while attempting to listen on port " + port);
+			System.out.println("Exception caught while attempting to listen on port " + port);
 			System.out.println(e.getMessage());
 		}
 		
 	}
 	public void listen() {
 		try (
-			Socket clientSocket = serverSocket.accept();     
+			Socket clientSocket = socket.accept();     
 			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
 			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			) {
